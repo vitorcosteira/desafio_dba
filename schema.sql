0bd70892b064847954b616c16933bd2de7b59f89
vitorcosteira <138516236+vitorcosteira@users.noreply.github.com>
2023-07-03 14:07:56 -0300

    Create schema.sql

---
+++ schema.sql
@@ -0,0 +1,207 @@
+USE [master]
+GO
+/****** Object:  Database [franquia_carros_v0]    Script Date: 03/07/2023 14:06:33 ******/
+CREATE DATABASE [franquia_carros_v0]
+ CONTAINMENT = NONE
+ ON  PRIMARY 
+( NAME = N'franquia_carros_v0', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL16.LABORATORIO\MSSQL\DATA\franquia_carros_v0.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
+ LOG ON 
+( NAME = N'franquia_carros_v0_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL16.LABORATORIO\MSSQL\DATA\franquia_carros_v0_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
+ WITH CATALOG_COLLATION = DATABASE_DEFAULT, LEDGER = OFF
+GO
+ALTER DATABASE [franquia_carros_v0] SET COMPATIBILITY_LEVEL = 160
+GO
+IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
+begin
+EXEC [franquia_carros_v0].[dbo].[sp_fulltext_database] @action = 'enable'
+end
+GO
+ALTER DATABASE [franquia_carros_v0] SET ANSI_NULL_DEFAULT OFF 
+GO
+ALTER DATABASE [franquia_carros_v0] SET ANSI_NULLS OFF 
+GO
+ALTER DATABASE [franquia_carros_v0] SET ANSI_PADDING OFF 
+GO
+ALTER DATABASE [franquia_carros_v0] SET ANSI_WARNINGS OFF 
+GO
+ALTER DATABASE [franquia_carros_v0] SET ARITHABORT OFF 
+GO
+ALTER DATABASE [franquia_carros_v0] SET AUTO_CLOSE OFF 
+GO
+ALTER DATABASE [franquia_carros_v0] SET AUTO_SHRINK OFF 
+GO
+ALTER DATABASE [franquia_carros_v0] SET AUTO_UPDATE_STATISTICS ON 
+GO
+ALTER DATABASE [franquia_carros_v0] SET CURSOR_CLOSE_ON_COMMIT OFF 
+GO
+ALTER DATABASE [franquia_carros_v0] SET CURSOR_DEFAULT  GLOBAL 
+GO
+ALTER DATABASE [franquia_carros_v0] SET CONCAT_NULL_YIELDS_NULL OFF 
+GO
+ALTER DATABASE [franquia_carros_v0] SET NUMERIC_ROUNDABORT OFF 
+GO
+ALTER DATABASE [franquia_carros_v0] SET QUOTED_IDENTIFIER OFF 
+GO
+ALTER DATABASE [franquia_carros_v0] SET RECURSIVE_TRIGGERS OFF 
+GO
+ALTER DATABASE [franquia_carros_v0] SET  ENABLE_BROKER 
+GO
+ALTER DATABASE [franquia_carros_v0] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
+GO
+ALTER DATABASE [franquia_carros_v0] SET DATE_CORRELATION_OPTIMIZATION OFF 
+GO
+ALTER DATABASE [franquia_carros_v0] SET TRUSTWORTHY OFF 
+GO
+ALTER DATABASE [franquia_carros_v0] SET ALLOW_SNAPSHOT_ISOLATION OFF 
+GO
+ALTER DATABASE [franquia_carros_v0] SET PARAMETERIZATION SIMPLE 
+GO
+ALTER DATABASE [franquia_carros_v0] SET READ_COMMITTED_SNAPSHOT OFF 
+GO
+ALTER DATABASE [franquia_carros_v0] SET HONOR_BROKER_PRIORITY OFF 
+GO
+ALTER DATABASE [franquia_carros_v0] SET RECOVERY FULL 
+GO
+ALTER DATABASE [franquia_carros_v0] SET  MULTI_USER 
+GO
+ALTER DATABASE [franquia_carros_v0] SET PAGE_VERIFY CHECKSUM  
+GO
+ALTER DATABASE [franquia_carros_v0] SET DB_CHAINING OFF 
+GO
+ALTER DATABASE [franquia_carros_v0] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
+GO
+ALTER DATABASE [franquia_carros_v0] SET TARGET_RECOVERY_TIME = 60 SECONDS 
+GO
+ALTER DATABASE [franquia_carros_v0] SET DELAYED_DURABILITY = DISABLED 
+GO
+ALTER DATABASE [franquia_carros_v0] SET ACCELERATED_DATABASE_RECOVERY = OFF  
+GO
+EXEC sys.sp_db_vardecimal_storage_format N'franquia_carros_v0', N'ON'
+GO
+ALTER DATABASE [franquia_carros_v0] SET QUERY_STORE = ON
+GO
+ALTER DATABASE [franquia_carros_v0] SET QUERY_STORE (OPERATION_MODE = READ_WRITE, CLEANUP_POLICY = (STALE_QUERY_THRESHOLD_DAYS = 30), DATA_FLUSH_INTERVAL_SECONDS = 900, INTERVAL_LENGTH_MINUTES = 60, MAX_STORAGE_SIZE_MB = 1000, QUERY_CAPTURE_MODE = AUTO, SIZE_BASED_CLEANUP_MODE = AUTO, MAX_PLANS_PER_QUERY = 200, WAIT_STATS_CAPTURE_MODE = ON)
+GO
+USE [franquia_carros_v0]
+GO
+/****** Object:  Table [dbo].[carros]    Script Date: 03/07/2023 14:06:33 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[carros](
+	[id] [int] NOT NULL,
+	[id_loja] [int] NULL,
+	[id_fornecedor] [int] NULL,
+	[modelo] [varchar](50) NULL,
+	[ano] [int] NULL,
+	[preco] [decimal](10, 0) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[clientes]    Script Date: 03/07/2023 14:06:33 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[clientes](
+	[id] [int] NOT NULL,
+	[nome] [varchar](50) NULL,
+	[endereco] [varchar](100) NULL,
+	[estado] [varchar](100) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[fornecedores]    Script Date: 03/07/2023 14:06:33 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[fornecedores](
+	[id] [int] NOT NULL,
+	[nome] [varchar](50) NULL,
+	[endereco] [varchar](100) NULL,
+	[estado] [varchar](100) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[funcionarios]    Script Date: 03/07/2023 14:06:33 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[funcionarios](
+	[id] [int] NOT NULL,
+	[id_loja] [int] NULL,
+	[nome] [varchar](50) NULL,
+	[cargo] [varchar](50) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[loja]    Script Date: 03/07/2023 14:06:33 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[loja](
+	[id] [int] NOT NULL,
+	[nome] [varchar](50) NULL,
+	[endereco] [varchar](100) NULL,
+	[estado] [varchar](100) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[vendas]    Script Date: 03/07/2023 14:06:33 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[vendas](
+	[id] [int] NOT NULL,
+	[id_carro] [int] NULL,
+	[id_cliente] [int] NULL,
+	[id_funcionario] [int] NULL,
+	[data] [date] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[carros]  WITH CHECK ADD FOREIGN KEY([id_fornecedor])
+REFERENCES [dbo].[fornecedores] ([id])
+GO
+ALTER TABLE [dbo].[carros]  WITH CHECK ADD FOREIGN KEY([id_loja])
+REFERENCES [dbo].[loja] ([id])
+GO
+ALTER TABLE [dbo].[funcionarios]  WITH CHECK ADD FOREIGN KEY([id_loja])
+REFERENCES [dbo].[loja] ([id])
+GO
+ALTER TABLE [dbo].[vendas]  WITH CHECK ADD FOREIGN KEY([id_carro])
+REFERENCES [dbo].[carros] ([id])
+GO
+ALTER TABLE [dbo].[vendas]  WITH CHECK ADD FOREIGN KEY([id_cliente])
+REFERENCES [dbo].[clientes] ([id])
+GO
+ALTER TABLE [dbo].[vendas]  WITH CHECK ADD FOREIGN KEY([id_funcionario])
+REFERENCES [dbo].[funcionarios] ([id])
+GO
+USE [master]
+GO
+ALTER DATABASE [franquia_carros_v0] SET  READ_WRITE 
+GO
